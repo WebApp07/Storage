8e610f7d3c2622771a5120aee03fab1635b64fc7
unknown <aminezouguig@proton.me>
2024-11-14 12:04:25 +0100

    feat: Implement OTP Modal for user authentication feat: Add OTP Modal with email verification and session management

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OtpModal from "./OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -143,7 +144,12 @@ const AuthForm = ({ type }: { type: FormType }) => {
           </div>
         </form>
       </Form>
+
       {/* OTP Verification */}
+
+      {accountId && (
+        <OtpModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
